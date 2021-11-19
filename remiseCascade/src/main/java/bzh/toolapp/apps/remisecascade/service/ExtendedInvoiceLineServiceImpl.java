a5444897e1b231984c259ceecf9302fd1f40e4d4
Gabriel Barbier <amelioretonparent@gmail.com>
2021-11-19 15:39:14 +0100

    Apply discounts from PriceList to Invoice.
    Add new method in InvoiceController to propage global discount amount.
    Replace existing field in Invoice.xml to be able to use our new method
    for PriceList changes.

--- remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/service/ExtendedInvoiceLineServiceImpl.java
+++ remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/service/ExtendedInvoiceLineServiceImpl.java
@@ -1,17 +1,23 @@
 package bzh.toolapp.apps.remisecascade.service;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
+import com.axelor.apps.account.db.Invoice;
 import com.axelor.apps.account.db.InvoiceLine;
 import com.axelor.apps.account.service.AccountManagementAccountService;
 import com.axelor.apps.account.service.AnalyticMoveLineService;
 import com.axelor.apps.account.service.app.AppAccountService;
 import com.axelor.apps.account.service.invoice.InvoiceLineService;
+import com.axelor.apps.base.db.PriceList;
+import com.axelor.apps.base.db.PriceListLine;
+import com.axelor.apps.base.db.repo.PriceListLineRepository;
 import com.axelor.apps.base.service.CurrencyService;
 import com.axelor.apps.base.service.PriceListService;
 import com.axelor.apps.base.service.ProductCompanyService;
 import com.axelor.apps.businessproject.service.InvoiceLineProjectServiceImpl;
 import com.axelor.apps.purchase.service.PurchaseProductService;
+import com.axelor.exception.AxelorException;
 import com.google.inject.Inject;
 
 public class ExtendedInvoiceLineServiceImpl extends InvoiceLineProjectServiceImpl implements InvoiceLineService {
@@ -39,5 +45,44 @@ public class ExtendedInvoiceLineServiceImpl extends InvoiceLineProjectServiceImp
 				invoiceLine.getSecDiscountTypeSelect(), invoiceLine.getSecDiscountAmount());
 
 		return secondDiscount;
+	}
+
+	@Override
+	public Map<String, Object> getDiscountsFromPriceLists(final Invoice invoice, final InvoiceLine invoiceLine,
+			final BigDecimal price) {
+
+		Map<String, Object> discounts = null;
+
+		final PriceList priceList = invoice.getPriceList();
+
+		if (priceList != null) {
+			final PriceListLine priceListLine = this.getPriceListLine(invoiceLine, priceList, price);
+			discounts = this.priceListService.getReplacedPriceAndDiscounts(priceList, priceListLine, price);
+			// and disable totally old behavior (remove deprecated entries)
+			discounts.put("discountAmount", BigDecimal.ZERO);
+			discounts.put("discountTypeSelect", PriceListLineRepository.AMOUNT_TYPE_NONE);
+		}
+
+		return discounts;
+	}
+
+	@Override
+	public Map<String, Object> getDiscount(final Invoice invoice, final InvoiceLine invoiceLine, final BigDecimal price)
+			throws AxelorException {
+		final Map<String, Object> processedDiscounts = super.getDiscount(invoice, invoiceLine, price);
+
+		// add behavior to manage activation of discounts from price list
+		final Map<String, Object> rawDiscounts = this.getDiscountsFromPriceLists(invoice, invoiceLine, price);
+		if (rawDiscounts != null) {
+			processedDiscounts.put("discountTypeSelect",
+					rawDiscounts.get(PriceListConstants.LINE_DISCOUNT_TYPE_SELECT));
+			processedDiscounts.put("discountAmount", rawDiscounts.get(PriceListConstants.LINE_DISCOUNT_AMOUNT));
+			processedDiscounts.put("secDiscountTypeSelect",
+					rawDiscounts.get(PriceListConstants.LINE_SECOND_DISCOUNT_TYPE_SELECT));
+			processedDiscounts.put("secDiscountAmount",
+					rawDiscounts.get(PriceListConstants.LINE_SECOND_DISCOUNT_AMOUNT));
+	}
+
+		return processedDiscounts;
 	}
 }
--- remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/InvoiceController.java
+++ remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/InvoiceController.java
@@ -2,6 +2,8 @@ package bzh.toolapp.apps.remisecascade.web;
 
 import com.axelor.apps.account.db.Invoice;
 import com.axelor.apps.account.service.invoice.InvoiceService;
+import com.axelor.apps.base.db.PriceList;
+import com.axelor.apps.base.db.repo.PriceListLineRepository;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
@@ -27,6 +29,21 @@ public class InvoiceController {
 			response.setValues(invoice);
 		} catch (final Exception e) {
 			TraceBackService.trace(response, e);
+		}
+	}
+
+	public void propagatePriceListDiscounts(final ActionRequest request, final ActionResponse response) {
+		final Invoice invoice = request.getContext().asType(Invoice.class);
+
+		final PriceList priceList = invoice.getPriceList();
+		if (priceList != null) {
+			// override global discount information
+			invoice.setDiscountAmount(priceList.getGeneralDiscount());
+			invoice.setDiscountTypeSelect(PriceListLineRepository.AMOUNT_TYPE_PERCENT);
+			invoice.setSecDiscountAmount(priceList.getSecGeneralDiscount());
+			invoice.setSecDiscountTypeSelect(PriceListLineRepository.AMOUNT_TYPE_PERCENT);
+			// send updated element to view
+			response.setValues(invoice);
 		}
 	}
 }
