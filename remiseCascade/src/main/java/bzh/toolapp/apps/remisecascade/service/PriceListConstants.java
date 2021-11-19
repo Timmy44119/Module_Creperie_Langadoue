c5dddcb9e6262cb8e9250f506084debd8177dad0
Gabriel Barbier <amelioretonparent@gmail.com>
2021-11-19 11:21:32 +0100

    Apply discounts from PriceList to SaleOrder.
    Extract constants into dedicated class.
    Use these constants to perform mapping between model and view (using
    discounts Map).
    Add new method in SaleOrderController to propage global discount amount.
    Replace existing field in SaleOrder.xml to be able to use our new method
    for PriceList changes.
    
    Conflicts:
    	remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/service/ExtendedSaleOrderLineServiceImpl.java

---
+++ remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/service/PriceListConstants.java
@@ -0,0 +1,17 @@
+package bzh.toolapp.apps.remisecascade.service;
+
+/**
+ * To share elements identifiers. For instance, discount keys...
+ */
+public class PriceListConstants {
+
+	static final String GLOBAL_SECOND_DISCOUNT_TYPE_SELECT = "globalSecondDiscountTypeSelect";
+	static final String GLOBAL_SECOND_DISCOUNT_AMOUNT = "globalSecondDiscountAmount";
+	static final String GLOBAL_DISCOUNT_TYPE_SELECT = "globalDiscountTypeSelect";
+	static final String GLOBAL_DISCOUNT_AMOUNT = "globalDiscountAmount";
+	public static final String LINE_SECOND_DISCOUNT_TYPE_SELECT = "lineSecondDiscountTypeSelect";
+	public static final String LINE_SECOND_DISCOUNT_AMOUNT = "lineSecondDiscountAmount";
+	public static final String LINE_DISCOUNT_TYPE_SELECT = "lineDiscountTypeSelect";
+	public static final String LINE_DISCOUNT_AMOUNT = "lineDiscountAmount";
+
+}
--- remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/SaleOrderController.java
+++ remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/SaleOrderController.java
@@ -1,5 +1,7 @@
 package bzh.toolapp.apps.remisecascade.web;
 
+import com.axelor.apps.base.db.PriceList;
+import com.axelor.apps.base.db.repo.PriceListLineRepository;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.service.saleorder.SaleOrderComputeService;
 import com.axelor.exception.service.TraceBackService;
@@ -10,15 +12,29 @@ import com.google.inject.Singleton;
 
 @Singleton
 public class SaleOrderController {
-  public void compute(final ActionRequest request, final ActionResponse response) {
+	public void compute(final ActionRequest request, final ActionResponse response) {
+		SaleOrder saleOrder = request.getContext().asType(SaleOrder.class);
 
-    SaleOrder saleOrder = request.getContext().asType(SaleOrder.class);
+		try {
+			saleOrder = Beans.get(SaleOrderComputeService.class).computeSaleOrder(saleOrder);
+			response.setValues(saleOrder);
+		} catch (final Exception e) {
+			TraceBackService.trace(response, e);
+		}
+	}
 
-    try {
-      saleOrder = Beans.get(SaleOrderComputeService.class).computeSaleOrder(saleOrder);
-      response.setValues(saleOrder);
-    } catch (final Exception e) {
-      TraceBackService.trace(response, e);
-    }
-  }
+	public void propagatePriceListDiscounts(final ActionRequest request, final ActionResponse response) {
+		final SaleOrder saleOrder = request.getContext().asType(SaleOrder.class);
+
+		final PriceList priceList = saleOrder.getPriceList();
+		if (priceList != null) {
+			// override global discount information
+			saleOrder.setDiscountAmount(priceList.getGeneralDiscount());
+			saleOrder.setDiscountTypeSelect(PriceListLineRepository.AMOUNT_TYPE_PERCENT);
+			saleOrder.setSecDiscountAmount(priceList.getSecGeneralDiscount());
+			saleOrder.setSecDiscountTypeSelect(PriceListLineRepository.AMOUNT_TYPE_PERCENT);
+			// send updated element to view
+			response.setValues(saleOrder);
+		}
+	}
 }
